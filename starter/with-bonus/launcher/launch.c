24f81f196432199373ac4b9134a6e822742d2c31
Rohak Kansal <110653229+xegtor@users.noreply.github.com>
2023-08-21 16:24:45 +0530

    Update launch.c

--- starter/with-bonus/launcher/launch.c
+++ starter/with-bonus/launcher/launch.c
@@ -11,9 +11,7 @@ int main(int argc, char** argv)
   FILE* elf_file = fopen(argv[1], "rb");
   if(elf_file){
     fread(&elf32Ehdr, sizeof(elf32Ehdr), 1, elf_file);
-    if (memcmp(elf32Ehdr.e_ident, ELFMAG, SELFMAG) == 0) {
-      printf("this is a valid elf file\n");
-    } else {printf("this is an invalid elf file\n"); exit(1);}
+    if (memcmp(elf32Ehdr.e_ident, ELFMAG, SELFMAG) != 0) {printf("this is an invalid elf file\n"); exit(1);}
     fclose(elf_file);
   }
   // 2. passing it to the loader for carrying out the loading/execution
